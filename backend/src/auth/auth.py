62161a6c331e6b832a5e4aa799f5e0c2e0c81464
a-yasinsky <anatoly@yasinsky.pro>
2020-02-08 14:33:03 +0600

    deleted common error handling

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -19,11 +19,6 @@ class AuthError(Exception):
         self.error = error
         self.status_code = status_code
 
-@APP.errorhandler(AuthError)
-def handle_auth_error(ex):
-    response = jsonify(ex.error)
-    response.status_code = ex.status_code
-    return response
 ## Auth Header
 
 '''
