582ded7a4dc6d12b5f2805b1d7f68a211cfa3756
a-yasinsky <anatoly@yasinsky.pro>
2020-02-10 16:54:38 +0600

    added decoding for urlopen

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -95,7 +95,7 @@ def check_permissions(permission, payload):
 '''
 def verify_decode_jwt(token):
     jsonurl = urlopen('https://{}/.well-known/jwks.json'.format(AUTH0_DOMAIN))
-    jwks = json.loads(jsonurl.read())
+    jwks = json.loads(jsonurl.read().decode('utf-8'))
     unverified_header = jwt.get_unverified_header(token)
     rsa_key = {}
     if 'kid' not in unverified_header:
